b94c6de79a79afdb496b80761ba8327cef52e0f9
wainou06 <wainou.6@gmail.com>
2025-08-08 15:15:59 +0900

    할 일 정리와 css 수정

---
+++ src/components/manager/managerUser.jsx
@@ -0,0 +1,5 @@
+function ManagerUser() {
+   return <>{/* mui table 쓰기 */}</>
+}
+
+export default ManagerUser
---
+++ src/components/manager/managerUserRating.jsx
@@ -0,0 +1,10 @@
+function ManagerUserRating() {
+   return (
+      <>
+         {/* 통계겠죠? */}
+         {/* mui에서 chart 불러오기! */}
+      </>
+   )
+}
+
+export default ManagerUserRating
